8b0ba5cce0cb82d3a2c57909b308c4fdc4eafe0a
mrawfulshadow <mrawfulshadow@gmail.com>
2026-08-18 02:34:45 +0530

    Student 1: Added Auth Service with Spring Security, JWT & Swagger

---
+++ auth-service/src/main/java/com/soc/authservice/model/User.java
@@ -0,0 +1,32 @@
+package com.soc.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String role;
+}
