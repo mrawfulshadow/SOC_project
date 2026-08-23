3aac46a44b02814c078403eddb1a16c35684e2fe
mrawfulshadow <mrawfulshadow@gmail.com>
2026-08-23 21:41:27 +0530

    Add jjwt-jackson dependency for JWT support in runtime

--- product-service/src/test/java/com/soc/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/soc/productservice/ProductServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.soc.productservice;
 
+import com.soc.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
+
+	@MockBean
+	private ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
